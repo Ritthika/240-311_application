7ea732a6a5a50218e643e467166e13437de0515a
DRTK <ritthikai2544@gmail.com>
2023-03-04 22:44:21 +0700

    basic first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import MyButton from './components/MyButton'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name , setName] =useState("TEST 240-311")
+  
+  const user = {
+    firstName : "admin",
+    lastName : "user",
+    imgPic : "https://i.ytimg.com/vi/nvYmvrnod18/maxresdefault.jpg"
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    //react Fragment
+    <> 
+      <h1>WELLCOME TO MY APP</h1>
+      <p>MY NAME is {user.firstName} {user.lastName}</p>
+      <img src={user.imgPic} width = "1280" height= "750" alt="" />
+      <p>{name}</p>
+      <MyButton />
+    </>
   )
 }
 
---
+++ Frontend/src/components/MyButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyButton() {
+  return (
+    <button>MyButton</button>
+  )
+}
+
+export default MyButton
