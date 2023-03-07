b13300bab39542f57555e5add2e74ad616359f6d
DRTK <ritthikai2544@gmail.com>
2023-03-07 12:10:43 +0700

    app-web and design commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,47 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import MyButton from './components/MyButton'
-import MyProfile from './components/MyProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name , setName] =useState("TEST 240-311")
-  
-  const user = {
-    firstName : "admin",
-    lastName : "user",
-    imgPic : "https://i.ytimg.com/vi/nvYmvrnod18/maxresdefault.jpg"
-  }
-  const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
-  ];
-
-  function handleClick(){
-    setCount (count + 1)
-  }
-  function handleClickde(){
-    setCount (count - 1)
-  }
-
   return (
-    //react Fragment
-    <> 
-      <h1>WELLCOME TO MY APP</h1>
-      <MyProfile data ={user}/>
-      <p>{name}</p>
-      <p>{count}</p>
-      <button onClick={handleClick}>Increase</button>
-      <button onClick={handleClickde}>decrease</button>
-      <MyButton />
-      <ul>
-        {products.map(product =>(
-          <li key={product.id}>{product.title}</li>
-        ))}
-      </ul>
-    </>
+  <div className="App">
+	<header className="app-header">
+		<img className="app-header-logo" src='/images/header.png ' />
+	</header>
+	<div className="app-grid">
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery01</h4>
+		</div>
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery02</h4>
+		</div>
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery03</h4>
+		</div>
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery04</h4>
+		</div>
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery05</h4>
+		</div>
+		<div className="lottery-item">
+			<img height="200px" src="/images/demo-lottery.jpg" /> 
+			<h4>Lottery06</h4>
+		</div>
+	</div>
+  </div> 
+   
   )
 }
 
