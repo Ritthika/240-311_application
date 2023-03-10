96a928a2530adeb6308ace95b096ab8133f45bac
Nattapat-Onkaew <6310110625@psu.ac.th>
2023-03-11 00:45:43 +0700

    config routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,11 +3,9 @@ const app = express();
 
 const products = require('../routes/products.js');
 const indexRouter = require('../routes/index');
-const usersRouter = require('../routes/users');
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/products', products);
 
 
